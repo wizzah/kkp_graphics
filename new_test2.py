879567aa5230bf3fb660714b01a221c0e75e9fae
Elizabeth Williams <elizabeth.williams@ucf.edu>
2016-05-23 23:24:16 -0400

    Works, but flickering gifs when overlapping

--- new_test2.py
+++ new_test2.py
@@ -4,6 +4,7 @@ import os
 
 batch = pyglet.graphics.Batch()
 test = pyglet.image.load_animation('fatcate.gif')
+other_test = pyglet.image.load_animation('../splash/1.gif')
 
 pyglet.resource.path = ['../splash']
 
@@ -13,11 +14,26 @@ total_height = window.height
 
 fps_display = pyglet.clock.ClockDisplay()
 
+def gen_coords(coord, img):
+	if coord == "x":
+		print img.frames[0].image.width
+		return randint(0, (total_width - img.frames[0].image.width))
+		# return randint(0, total_width)
+	else:
+		print (total_height - img.frames[0].image.height)
+		return randint(0, (total_height - img.frames[0].image.height))
+gen_coords("x", test)
+
 #if random is too slow, make a list of set points inside the bounding box
 
+
 #randomly generate coordinates in the screen boundary
-sprites = [pyglet.sprite.Sprite(test, batch=batch, x=randint(0, total_width), y=randint(0, total_height)),
-           pyglet.sprite.Sprite(pyglet.image.load_animation('../splash/1.gif'), batch=batch, x=randint(0, total_width), y=randint(0, total_height))]
+sprites = [
+	pyglet.sprite.Sprite(test, batch=batch, x=gen_coords("x", test), y=gen_coords("y", test)),
+	pyglet.sprite.Sprite(other_test, batch=batch, x=gen_coords("x", other_test), y=gen_coords("y", other_test)),
+	pyglet.sprite.Sprite(test, batch=batch, x=gen_coords("x", test), y=gen_coords("y", test)),
+	pyglet.sprite.Sprite(other_test, batch=batch, x=gen_coords("x", other_test), y=gen_coords("y", other_test))
+]
 
 # height = int(test.get_max_height())
 # width = int(test.get_max_width())
@@ -27,13 +43,13 @@ sprites = [pyglet.sprite.Sprite(test, batch=batch, x=randint(0, total_width), y=
 window.set_caption("hi")
 # set bg color
 # pyglet.clock.schedule_interval(self.update, 1.0/FPS)
+pyglet.gl.glClearColor(0,0,0,0)
 
 @window.event
 def on_draw():
     # window.clear()
     # window.draw()
     # sprites.draw()
-    pyglet.gl.glClearColor(0,0,0,0)
     window.clear()
     fps_display.draw()
     batch.draw()
